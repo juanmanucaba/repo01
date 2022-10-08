da46aa8ceaa4949abc097794ba235dbdf1b46333
manu_2022 <juanmanuelcaballero@hotmail.com>
2022-10-07 23:24:38 -0300

    agregue archivo script

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var edad = prompt("Ingrese su edad");
+
+if (edad >= 18){
+
+    if (edad >= 60){
+        alert("eres adulto mayor");
+    }else{
+        alert("eres mayor");
+    } 
+    
+} else {
+    alert("eres menor");
+}
+
+
+
+
+alert("Fin sistema");
+
